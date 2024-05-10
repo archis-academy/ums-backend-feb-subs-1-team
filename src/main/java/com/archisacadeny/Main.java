2c3c49a9cf94ad2d2bae9b64969ad57831e9ae4b
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-05-10 18:56:09 +0300

    create person class

--- src/main/java/com/archisacadeny/Main.java
+++ src/main/java/com/archisacadeny/Main.java
@@ -2,9 +2,11 @@ package com.archisacadeny;
 
 import com.archisacadeny.config.DataBaseConfig;
 import com.archisacadeny.config.DataBaseConnectorConfig;
+import com.archisacadeny.person.PersonRepository;
 
 public class Main {
     public static void main(String[] args) {
         DataBaseConnectorConfig.setConnection();
+        PersonRepository.createPersonTable();
     }
 }
