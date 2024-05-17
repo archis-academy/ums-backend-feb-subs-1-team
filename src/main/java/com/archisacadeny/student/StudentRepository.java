9d13fc01f44ce54c5452af8d09bddb6ef6c29353
gorkemiskeceli <mgiskec@gmail.com>
2024-05-17 18:19:23 +0300

    update

--- src/main/java/com/archisacadeny/student/StudentRepository.java
+++ src/main/java/com/archisacadeny/student/StudentRepository.java
@@ -12,7 +12,7 @@ public class StudentRepository {
         {
             String query = "DROP SEQUENCE IF EXITS student_id_seq;"+
                     "CREATE SEQUENCE student_id_seq INCREMENT BY 1 MINVALUE 0 MAXVALUE 2147483647 START 1;"+
-                    "CREATE TABLE IF NOT EXITS students" +
+                    "CREATE TABLE IF NOT EXITS students(" +
                     "id INTEGER DEFAULT nexval('student_id_seq') PRIMARY KEY," +
                     "full_name VARCHAR(255)," +
                     "email VARCHAR(255)," +
@@ -21,7 +21,7 @@ public class StudentRepository {
                     "student_number INTEGER,"+
                     "department VARCHAR(255),"+
                     "gender VARCHAR(255),"+
-                    "enrolled_course VARCHAR(255)";
+                    "enrolled_course VARCHAR(255))";
 
             statement.execute(query);
             System.out.println("Student table has been created in the database...");
