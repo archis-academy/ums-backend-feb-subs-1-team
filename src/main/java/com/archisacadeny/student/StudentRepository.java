d9305c5f36cae4dfda93e9ec68d8c1333c82c47d
yncrahmet <yeniceriahmet02@gmail.com>
2024-05-17 17:43:59 +0300

    [UM-08]-List-students-enrolled-course

--- src/main/java/com/archisacadeny/student/StudentRepository.java
+++ src/main/java/com/archisacadeny/student/StudentRepository.java
@@ -5,15 +5,4 @@ import java.util.List;
 public interface StudentRepository {
 
     List<Student> findByCourseId(int courseId);
-
-    List<Student> findAll();
-
-    Student save(Student student);
-
-    Student findById(int id);
-
-    void update(Student student);
-
-    void delete(int id);
-
 }
