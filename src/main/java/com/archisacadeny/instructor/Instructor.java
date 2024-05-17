9903f8d401ecf1bca8bac0316cc73605e6b533b2
Nehir0109 <nehir.cakilci@gmail.com>
2024-05-17 15:07:02 +0300

    nehir/[UM-1]-created instructor class

--- src/main/java/com/archisacadeny/instructor/Instructor.java
+++ src/main/java/com/archisacadeny/instructor/Instructor.java
@@ -1,4 +1,68 @@
 package com.archisacadeny.instructor;
 
+import com.archisacadeny.course.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Instructor {
+
+    private long id;
+    private String instructorName;
+    private String instructorNumber;
+    private String instructorEmail;
+    private String instructorPassword;
+    private List<Course> courses;
+
+    public Instructor() {
+        this.courses = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorNumber() {
+        return instructorNumber;
+    }
+
+    public void setInstructorNumber(String instructorNumber) {
+        this.instructorNumber = instructorNumber;
+    }
+
+    public String getInstructorEmail() {
+        return instructorEmail;
+    }
+
+    public void setInstructorEmail(String instructorEmail) {
+        this.instructorEmail = instructorEmail;
+    }
+
+    public String getInstructorPassword() {
+        return instructorPassword;
+    }
+
+    public void setInstructorPassword(String instructorPassword) {
+        this.instructorPassword = instructorPassword;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
 }
