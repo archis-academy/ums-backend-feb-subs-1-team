30a3e081bfeca9e90301d96e73c3d860632a00c1
yncrahmet <yeniceriahmet02@gmail.com>
2024-05-24 19:32:54 +0300

    [UM-08]-student.java

--- src/main/java/com/archisacadeny/student/Student.java
+++ src/main/java/com/archisacadeny/student/Student.java
@@ -1,57 +1,46 @@
 package com.archisacadeny.student;
 
+import com.archisacadeny.course.Course;
+
 import java.time.LocalDate;
+import java.util.List;
 
 public class Student {
-    private int id;
-    private final String fullName;
-    private final String gender;
-    private final int courseId;
-    private final String identityNo;
-    private final LocalDate unvEnrollmentDate;
-    private final int yearOfStudy;
-    private final int totalCreditCount;
+    private long id;
+    private String fullName;
+    private String gender;
+    private String identityNo;
+    private LocalDate enrollmentDate;
+    private List<Course> enrolledCourses;
+    private int yearOfStudy;
+    private int totalCreditCount;
 
-    public Student(int id, String fullName, String gender, int courseId, String identityNo, LocalDate unvEnrollmentDate, int yearOfStudy, int totalCreditCount) {
+    public Student(long id, String fullName, String gender, String identityNo, LocalDate enrollmentDate, int yearOfStudy, int totalCreditCount) {
         this.id = id;
         this.fullName = fullName;
         this.gender = gender;
-        this.courseId = courseId;
         this.identityNo = identityNo;
-        this.unvEnrollmentDate = unvEnrollmentDate;
+        this.enrollmentDate = enrollmentDate;
         this.yearOfStudy = yearOfStudy;
         this.totalCreditCount = totalCreditCount;
+        this.enrolledCourses = enrolledCourses;
     }
+    public long getId() { return id; }
 
-    public String getFullName() {
-        return fullName;
-    }
+    public String getFullName() { return fullName; }
 
-    public String getGender() {
-        return gender;
-    }
+    public String getGender() { return gender; }
 
-    public int getCourseId() {
-        return courseId;
-    }
+    public List<Course> getEnrolledCourses(int i) { return enrolledCourses; }
 
-    public String getIdentityNo() {
-        return identityNo;
-    }
+    public String getIdentityNo() { return identityNo; }
 
-    public LocalDate getUnvEnrollmentDate() {
-        return unvEnrollmentDate;
-    }
+    public LocalDate getEnrollmentDate() { return enrollmentDate; }
 
-    public int getYearOfStudy() {
-        return yearOfStudy;
-    }
+    public int getYearOfStudy() { return yearOfStudy; }
 
-    public int getTotalCreditCount() {
-        return totalCreditCount;
-    }
+    public int getTotalCreditCount() { return totalCreditCount; }
 
-    public int getId() {
-        return id;
+    public void setEnrolledCourses(List<Course> enrolledCourses) {
     }
 }
