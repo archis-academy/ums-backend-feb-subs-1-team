423a599f35f198b92a3dab20dd3b3ae8a0177cb8
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-24 16:31:59 +0300

    update method added

--- src/main/java/com/archisacadeny/course/CourseRepository.java
+++ src/main/java/com/archisacadeny/course/CourseRepository.java
@@ -17,5 +17,16 @@ public class CourseRepository {
         }
     }
 
+    public static void update(String courseNumber, Course course ){
+        String query = String.format(
+                "UPDATE courses SET name= '%1$s', number = '%2$s', instructor_id = '%3$s' WHERE number= '"+courseNumber+"'",
+                course.getCourseName(),course.getCourseNumber(),course.getInstructorId());
+        System.out.println(query);
+        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+            statement.execute();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
 
 }
