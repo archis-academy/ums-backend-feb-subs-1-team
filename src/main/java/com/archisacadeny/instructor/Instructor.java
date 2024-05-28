a7948d088d661c37ebed42cd503f189c3d03646e
Nehir0109 <nehir.cakilci@gmail.com>
2024-05-28 16:43:11 +0300

    nehir/[UM-1]-repository and service classes added

--- src/main/java/com/archisacadeny/instructor/Instructor.java
+++ src/main/java/com/archisacadeny/instructor/Instructor.java
@@ -1,17 +1,15 @@
 package com.archisacadeny.instructor;
 
 import com.archisacadeny.course.Course;
+import com.archisacadeny.person.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Instructor {
+public class Instructor extends Person {
 
     private long id;
-    private String name;
     private String number;
-    private String email;
-    private String password;
     private List<Course> courses;
 
     public Instructor() {this.courses = new ArrayList<>();}
@@ -19,17 +17,8 @@ public class Instructor {
     public long getId() {return id;}
     public void setId(long id) {this.id = id;}
 
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
-
     public String getNumber() {return number;}
     public void setNumber(String number) {this.number = number;}
-
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-
-    public String getPassword() {return password;}
-    public void setPassword(String password) {this.password = password;}
 
     public List<Course> getCourses() {return courses;}
     public void setCourses(List<Course> courses) {this.courses = courses;}
---
+++ src/main/java/com/archisacadeny/instructor/InstructorService.java
@@ -0,0 +1,13 @@
+package com.archisacadeny.instructor;
+
+public class InstructorService {
+    private final InstructorRepository instructorRepository;
+
+    public InstructorService(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
+    }
+
+    public Instructor createInstructor(Instructor instructor) {
+        return instructorRepository.save(instructor);
+    }
+}
