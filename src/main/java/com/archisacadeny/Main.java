3fd88aaaccdba1e9b4486a18ec35f32d5ed44ff2
Harutyun Kalataş <H.kalatas@gmail.com>
2024-05-28 16:35:28 +0300

    update

--- src/main/java/com/archisacadeny/Main.java
+++ src/main/java/com/archisacadeny/Main.java
@@ -1,12 +1,14 @@
 package com.archisacadeny;
 
 import com.archisacadeny.config.DataBaseConnectorConfig;
+import com.archisacadeny.courseStudentMapper.CourseStudentMapperRepository;
 import com.archisacadeny.person.PersonRepository;
 
 public class Main {
     public static void main(String[] args) {
         DataBaseConnectorConfig.setConnection();
         PersonRepository.createPersonTable();
+        CourseStudentMapperRepository.createCourseStudentTable();
 
         }
 }
