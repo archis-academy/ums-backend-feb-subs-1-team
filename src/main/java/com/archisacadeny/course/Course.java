816c3012cc3ed5e9e1335d7b3aff28e986e750a5
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-28 20:08:20 +0300

    [UM-5] COURSE class deleted

--- src/main/java/com/archisacadeny/course/Course.java
+++ src/main/java/com/archisacadeny/course/Course.java
@@ -7,46 +7,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Course {
-//    private long id;
-//    private String name;
-//    private String number;
-//    private String instructorId;
-//
-//    //private String enrolledStudents;
-//
-//    public long getId() {
-//        return id;
-//    }
-//
-//    public void setId(long id) {
-//        this.id = id;
-//    }
-//
-//    public String getCourseName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getCourseNumber() {
-//        return number;
-//    }
-//
-//    public void setNumber(String number) {
-//        this.number = number;
-//    }
-//
-//    public String getInstructorId() {
-//        return instructorId;
-//    }
-//
-//    public void setInstructorId(String instructorId) {
-//        this.instructorId = instructorId;
-//    }
-
-}
 
 
 }
+
+
+
