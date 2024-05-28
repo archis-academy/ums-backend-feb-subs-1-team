9a1ebf6ead851466477e16f6f63e9a31116fbd74
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-05-28 11:37:33 +0300

    fields added

--- src/main/java/com/archisacadeny/course/CourseRepository.java
+++ src/main/java/com/archisacadeny/course/CourseRepository.java
@@ -19,8 +19,26 @@ public class CourseRepository {
 
     public static void update(String courseNumber, Course course ){
         String query = String.format(
-                "UPDATE courses SET name= '%1$s', number = '%2$s', instructor_id = '%3$s' WHERE number= '"+courseNumber+"'",
-                course.getCourseName(),course.getCourseNumber(),course.getInstructorId());
+                "UPDATE courses SET name= '%1$s'," +
+                        " number = '%2$s'," +
+                        " instructor_id = '%3$s' " +
+                        " creditHours = '%4$s' " +
+                        " totalStudent = '%5$s' "+
+                        " enrolledStudent = '%6$s' "+
+                        " department = '%7$s' "+
+                        " maxStudents = '%8$s'"+
+
+                        "WHERE number= '"+courseNumber+"'",
+                course.getCourseName(),
+                course.getCourseNumber(),
+                course.getInstructor().getId(),
+                course.getCreditHours(),
+                course.getTotalStudent(),
+                course.getEnrolledStudent(),
+                course.getEnrolledStudent(),
+                course.getDepartment(),
+                course.getMaxStudents()
+                );
         System.out.println(query);
         try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
             statement.execute();
