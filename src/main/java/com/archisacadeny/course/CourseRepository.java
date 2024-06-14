31fd2d687b68e9d305a78d7d47f68dccea0cf523
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-06-14 20:20:29 +0300

    Merge with master

--- src/main/java/com/archisacadeny/course/CourseRepository.java
+++ src/main/java/com/archisacadeny/course/CourseRepository.java
@@ -5,23 +5,22 @@ import com.archisacadeny.config.DataBaseConnectorConfig;
 import java.sql.*;
 
 public class CourseRepository {
-    public static void createCourseTable(){
-        try(Statement statement = DataBaseConnectorConfig.getConnection().createStatement())
-        {
+    public static void createCourseTable() {
+        try (Statement statement = DataBaseConnectorConfig.getConnection().createStatement()) {
             String query = """
-                    DROP SEQUENCE IF EXISTS course_id_seq;
-                    CREATE SEQUENCE course_id_seq INCREMENT BY 1 MINVALUE 0 MAXVALUE 2147483647 START 1;
-                    CREATE TABLE IF NOT EXISTS courses(
-                    "id" INTEGER DEFAULT nextval('course_id_seq') PRIMARY KEY NOT NULL,
-                    "name" VARCHAR(255) NOT NULL,
-                    "number" VARCHAR(255) NOT NULL,
-                    "credits" INTEGER,
-                    "department" VARCHAR(255),
-                    "max_students" INTEGER,
-                    "instructor_id" INTEGER,
-                    CONSTRAINT fk_instructor_id FOREIGN KEY (instructor_id) REFERENCES "public"."instructors"(id)
-                    )
-            """;
+                            DROP SEQUENCE IF EXISTS course_id_seq;
+                            CREATE SEQUENCE course_id_seq INCREMENT BY 1 MINVALUE 0 MAXVALUE 2147483647 START 1;
+                            CREATE TABLE IF NOT EXISTS courses(
+                            "id" INTEGER DEFAULT nextval('course_id_seq') PRIMARY KEY NOT NULL,
+                            "name" VARCHAR(255) NOT NULL,
+                            "number" VARCHAR(255) NOT NULL,
+                            "credits" INTEGER,
+                            "department" VARCHAR(255),
+                            "max_students" INTEGER,
+                            "instructor_id" INTEGER,
+                            CONSTRAINT fk_instructor_id FOREIGN KEY (instructor_id) REFERENCES "public"."instructors"(id)
+                            )
+                    """;
             statement.execute(query);
             System.out.println("Courses table has been created in the database..");
 
@@ -30,20 +29,20 @@ public class CourseRepository {
         }
     }
 
-    public static Course save(Course course){
+    public static Course save(Course course) {
         String query = "INSERT INTO courses(name,number,credits,department,max_students,instructor_id) VALUES(?,?,?,?,?,?)";
-        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
-            statement.setString(1,course.getCourseName());
-            statement.setString(2,course.getCourseNumber());
-            statement.setLong(3,course.getCredits());
-            statement.setString(4,course.getDepartment());
-            statement.setLong(5,course.getMaxStudents());
-            statement.setLong(6,course.getInstructor().getId());
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)) {
+            statement.setString(1, course.getCourseName());
+            statement.setString(2, course.getCourseNumber());
+            statement.setLong(3, course.getCredits());
+            statement.setString(4, course.getDepartment());
+            statement.setLong(5, course.getMaxStudents());
+            statement.setLong(6, course.getInstructor().getId());
 
             statement.execute();
-            System.out.println("Course has been saved successfully with name: "+course.getCourseName());
+            System.out.println("Course has been saved successfully with name: " + course.getCourseName());
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return course;
@@ -52,10 +51,10 @@ public class CourseRepository {
 
     public static void deleteCourse(long courseId) {
         String query = "DELETE FROM \"courses\"" +
-                "WHERE id = '"+courseId+"'";
-        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+                "WHERE id = '" + courseId + "'";
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)) {
             statement.execute();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
@@ -66,34 +65,33 @@ public class CourseRepository {
         int maxStudents = 0;
 
         String query = "SELECT \"courses\".id, name , max_students, COUNT(course_student_mapper.course_id) as n_of_students FROM \"courses\"" +
-                "LEFT JOIN \"course_student_mapper\" ON  course_student_mapper.course_id = \"courses\".\"id\""+
-                "WHERE courses.id = '"+courseId + "' GROUP BY courses.id  ";
+                "LEFT JOIN \"course_student_mapper\" ON  course_student_mapper.course_id = \"courses\".\"id\"" +
+                "WHERE courses.id = '" + courseId + "' GROUP BY courses.id  ";
 
 
-        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)) {
             statement.execute();
             ResultSet rs = statement.getResultSet();
             while (rs.next()) {
                 studentCount = rs.getInt("n_of_students");
                 maxStudents = rs.getInt("max_students");
-                System.out.println(studentCount + "A"+maxStudents);
+                System.out.println(studentCount + "A" + maxStudents);
             }
             printResultSet(rs);
             //PRINTLENMIYOR, rs.next() bittikten sonra.
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
-        if( studentCount == maxStudents){
+        if (studentCount == maxStudents) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public static void printResultSet(ResultSet rs) throws SQLException
-    {
+    public static void printResultSet(ResultSet rs) throws SQLException {
         ResultSetMetaData rsmd = rs.getMetaData();
         int columnsNumber = rsmd.getColumnCount();
         while (rs.next()) {
@@ -106,27 +104,27 @@ public class CourseRepository {
     }
 
 
-    public static void update(String courseNumber, Course course ){
+    public static void update(String courseNumber, Course course) {
         String query = String.format(
                 "UPDATE courses SET name= '%1$s'," +
                         " number = '%2$s'," +
                         " instructor_id = '%3$s' " +
                         " credits = '%4$s' " +
-                        " department = '%5$s' "+
-                        " max_students = '%6$s'"+
+                        " department = '%5$s' " +
+                        " max_students = '%6$s'" +
 
-                        "WHERE number= '"+courseNumber+"'",
+                        "WHERE number= '" + courseNumber + "'",
                 course.getCourseName(),
                 course.getCourseNumber(),
                 course.getInstructor().getId(),
                 course.getCredits(),
                 course.getDepartment(),
                 course.getMaxStudents()
-                );
+        );
         System.out.println(query);
-        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)) {
             statement.execute();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
@@ -134,10 +132,10 @@ public class CourseRepository {
     public static double getTotalCreditAmount(long studentId) {
         double count = 0.0;
         String query = "SELECT  student_id , credits  FROM \"course_student_mapper\"" +
-                "LEFT JOIN \"courses\"  ON  course_student_mapper.course_id = \"courses\".\"id\""+
-                "WHERE student_id = '"+studentId + "'";
+                "LEFT JOIN \"courses\"  ON  course_student_mapper.course_id = \"courses\".\"id\"" +
+                "WHERE student_id = '" + studentId + "'";
 
-        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)) {
             statement.execute();
             ResultSet rs = statement.getResultSet();
             while (rs.next()) {
@@ -147,7 +145,7 @@ public class CourseRepository {
 //            printResultSet(rs);
             //PRINTLENMIYOR, rs.next() bittikten sonra.
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return count;
@@ -160,14 +158,14 @@ public class CourseRepository {
                 "GROUP BY course_id " +
                 "ORDER BY student_count DESC LIMIT 1 ";
 
-        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)) {
             statement.execute();
             ResultSet rs = statement.getResultSet();
             while (rs.next()) {
                 courseId = rs.getInt("course_id");
             }
 //            printResultSet(rs);
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return courseId;
@@ -182,7 +180,7 @@ public class CourseRepository {
 
         int[] ids;
         int count = 0;
-        //a
+
         try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE,
                 ResultSet.CONCUR_UPDATABLE)) {
             statement.execute();
