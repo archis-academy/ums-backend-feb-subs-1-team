e3f23d5e228727b1c856dced0775524c61fba0b9
Nehir0109 <nehir.cakilci@gmail.com>
2024-06-17 14:56:25 +0300

    nehir/[UM-57]-crud methods for instructor has been added

--- src/main/java/com/archisacadeny/instructor/InstructorService.java
+++ src/main/java/com/archisacadeny/instructor/InstructorService.java
@@ -19,4 +19,19 @@ public class InstructorService {
     public List<Instructor> listAllInstructors() {
         return instructorRepository.listAllInstructors();
     }
+
+    public Instructor getInstructorById(long instructorId){
+        return instructorRepository.getInstructorById(instructorId);
+    }
+
+    public Instructor updateInstructor(Instructor instructor) {
+        if(instructor == null){
+            throw new RuntimeException("instructor is null");
+        }
+        return instructorRepository.updateInstructor(instructor);
+    }
+
+    public void deleteInstructor(long instructorId) {
+        instructorRepository.deleteInstructor(instructorId);
+    }
 }
