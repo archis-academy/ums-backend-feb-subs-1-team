e04ad97640ba8503f24a0907e1e86fe6aee8f1db
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-06-18 17:24:56 +0300

    added main [UM-14] class

---
+++ src/main/java/com/archisacadeny/Main.java
@@ -0,0 +1,28 @@
+package com.archisacadeny;
+
+import com.archisacadeny.config.DataBaseConfig;
+import com.archisacadeny.config.DataBaseConnectorConfig;
+import com.archisacadeny.course.Course;
+//import com.archisacadeny.course.CourseRepository;
+import com.archisacadeny.course.CourseRepository;
+import com.archisacadeny.course.CourseService;
+import com.archisacadeny.instructor.Instructor;
+import com.archisacadeny.instructor.InstructorRepository;
+import com.archisacadeny.instructor.InstructorService;
+import com.archisacadeny.person.Person;
+import com.archisacadeny.person.PersonRepository;
+import com.archisacadeny.person.PersonService;
+import com.archisacadeny.student.CourseStudentMapper;
+import com.archisacadeny.student.Student;
+import com.archisacadeny.student.StudentRepository;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        DataBaseConnectorConfig.setConnection();
+    }
+}
