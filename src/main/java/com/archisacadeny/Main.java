c96ad28d658e0fda369a0e518f5d0ecf44da13b7
Görkem İskeçeli <gorkemiskeceli@Gorkem-MacBook-Air.local>
2024-06-21 13:45:45 +0300

    confict fix

--- src/main/java/com/archisacadeny/Main.java
+++ src/main/java/com/archisacadeny/Main.java
@@ -1,24 +1,12 @@
 package com.archisacadeny;
 
-import com.archisacadeny.config.DataBaseConfig;
 import com.archisacadeny.config.DataBaseConnectorConfig;
-
-import com.archisacadeny.course.Course;
-import com.archisacadeny.course.CourseRepository;
-
-import com.archisacadeny.person.Person;
-
-import com.archisacadeny.person.PersonRepository;
-import com.archisacadeny.person.PersonService;
-import com.archisacadeny.student.Student;
-import com.archisacadeny.student.StudentRepository;
-
-import java.sql.Timestamp;
 
 public class Main {
     public static void main(String[] args) {
         DataBaseConnectorConfig.setConnection();
 
-        //PersonRepository.createPersonTable();
+
+
     }
 }
