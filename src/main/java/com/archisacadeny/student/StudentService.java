6e57492703b4289432680350132ee54c6cdc2934
Görkem İskeçeli <gorkemiskeceli@Gorkem-MacBook-Air.local>
2024-06-25 21:36:34 +0300

    Services has been updated

--- src/main/java/com/archisacadeny/student/StudentService.java
+++ src/main/java/com/archisacadeny/student/StudentService.java
@@ -3,13 +3,28 @@ package com.archisacadeny.student;
 public class StudentService {
     private final StudentRepository studentRepository;
 
+    public void viewStudentDetails(Student student){
+
+        System.out.println("Full Name:" + student.getFullName());
+        System.out.println("Gender:" + student.getGender());
+        System.out.println("Identity No:" + student.getIdentityNo());
+        System.out.println("Enrollment Date:" + student.getEnrollmentDate());
+        System.out.println("Years of study:" + student.getYearOfStudy());
+        System.out.println("Total Credits:" + student.getTotalCreditCount());
+    }
     public StudentService(StudentRepository studentRepository){
         this.studentRepository=studentRepository;
     }
     public Student createStudent(Student student){
+
         return studentRepository.createStudent(student);
     }
-    public Student updateStudentInfo(Student student){
-        return studentRepository.updateStudentInfo(student);
+    public void updateStudentInfo(Student student){
+
+        System.out.println("Full Name:" + student.getFullName());
+        System.out.println("Gender:" + student.getGender());
+        System.out.println("Identity No:" + student.getIdentityNo());
+        System.out.println("Enrollment Date:" + student.getEnrollmentDate());
+
     }
 }
