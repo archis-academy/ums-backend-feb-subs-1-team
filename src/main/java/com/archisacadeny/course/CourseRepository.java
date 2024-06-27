e6499ae8ccd8a96a12c2e3a3dd6ece5b74abb128
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-06-28 01:19:26 +0300

    [UM-10] Changed order of code

--- src/main/java/com/archisacadeny/course/CourseRepository.java
+++ src/main/java/com/archisacadeny/course/CourseRepository.java
@@ -179,7 +179,30 @@ public class CourseRepository {
         return courseId;
     }
 
-public List<Course> getAllCourses() {
+  public static Course getCourseById(long courseId){
+        String query = "SELECT * FROM courses WHERE id = "+courseId;
+        Course course = null;
+        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+            statement.execute();
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                course = new Course(courseId,
+                        rs.getString("name")
+                        , InstructorRepository.getInstructorById( rs.getLong("instructor_id"))
+                        ,rs.getLong("credits")
+                        ,rs.getString("number")
+                        , getCourseEnrolledStudents(courseId)
+                        ,rs.getString("department")
+                        ,rs.getInt("max_students"));
+            }
+            //printResultSet(rs);
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+        return course;
+    }
+
+    public List<Course> getAllCourses() {
         ArrayList<Course> courses = new ArrayList<>();
         String query = "SELECT * FROM \"courses\" ";
 
@@ -205,28 +228,5 @@ public List<Course> getAllCourses() {
             throw new RuntimeException(e);
         }
         return courses;
-    }
-  
-  public static Course getCourseById(long courseId){
-        String query = "SELECT * FROM courses WHERE id = "+courseId;
-        Course course = null;
-        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
-            statement.execute();
-            ResultSet rs = statement.getResultSet();
-            while (rs.next()) {
-                course = new Course(courseId,
-                        rs.getString("name")
-                        , InstructorRepository.getInstructorById( rs.getLong("instructor_id"))
-                        ,rs.getLong("credits")
-                        ,rs.getString("number")
-                        , getCourseEnrolledStudents(courseId)
-                        ,rs.getString("department")
-                        ,rs.getInt("max_students"));
-            }
-            //printResultSet(rs);
-        }catch(SQLException e){
-            throw new RuntimeException(e);
-        }
-        return course;
     }
 }
