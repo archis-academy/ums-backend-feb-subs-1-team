cc21edddd19d8b1f755aa8fe6d9d003e2b9ba629
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-06-28 00:58:19 +0300

    [UM-31] uncomment

--- src/main/java/com/archisacadeny/course/CourseRepository.java
+++ src/main/java/com/archisacadeny/course/CourseRepository.java
@@ -176,28 +176,28 @@ public class CourseRepository {
         return courseId;
     }
 
-//    public static Course getCourseById(long courseId){
-//        String query = "SELECT * FROM courses WHERE id = "+courseId;
-//        Course course = null;
-//        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
-//            statement.execute();
-//            ResultSet rs = statement.getResultSet();
-//            while (rs.next()) {
-//                course = new Course(courseId,
-//                        rs.getString("name")
-//                        , InstructorRepository.getInstructorById( rs.getLong("instructor_id"))
-//                        ,rs.getLong("credits")
-//                        ,rs.getString("number")
-//                        , getCourseEnrolledStudents(courseId)
-//                        ,rs.getString("department")
-//                        ,rs.getInt("max_students"));
-//            }
-//            //printResultSet(rs);
-//        }catch(SQLException e){
-//            throw new RuntimeException(e);
-//        }
-//        return course;
-//    }
+    public static Course getCourseById(long courseId){
+        String query = "SELECT * FROM courses WHERE id = "+courseId;
+        Course course = null;
+        try(PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+            statement.execute();
+            ResultSet rs = statement.getResultSet();
+            while (rs.next()) {
+                course = new Course(courseId,
+                        rs.getString("name")
+                        , InstructorRepository.getInstructorById( rs.getLong("instructor_id"))
+                        ,rs.getLong("credits")
+                        ,rs.getString("number")
+                        , getCourseEnrolledStudents(courseId)
+                        ,rs.getString("department")
+                        ,rs.getInt("max_students"));
+            }
+            //printResultSet(rs);
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+        return course;
+    }
 
     public static Student findTopStudentInInstructorCourses(int instructorId) {
         String query = "SELECT student_id, grade, courses.instructor_id, " +
