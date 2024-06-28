3d8097dd5d5d475115ee660e462799b268dd0069
Görkem İskeçeli <gorkemiskeceli@Gorkem-MacBook-Air.local>
2024-06-28 05:25:21 +0300

    UM-44-update-student method added

--- src/main/java/com/archisacadeny/student/StudentRepository.java
+++ src/main/java/com/archisacadeny/student/StudentRepository.java
@@ -4,6 +4,8 @@ import com.archisacadeny.config.DataBaseConnectorConfig;
 import java.sql.*;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentRepository {
 
@@ -116,6 +118,52 @@ public class StudentRepository {
             throw new RuntimeException(e);
         }
         return student;
+    }
+    public Student updateStudentInfo(Student student) {
+        String query = "UPDATE students SET full_name = ?, gender = ?, identity_no = ?, enrollment_date = ? WHERE id = ?";
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)) {
+
+            statement.setString(1, student.getFullName());
+            statement.setString(2, student.getGender());
+            statement.setString(3, student.getIdentityNo());
+            statement.setTimestamp(4, student.getEnrollmentDate());
+            statement.setLong(5, student.getId());
+
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("The Student you have reach by id has been update" + student.getId());
+            } else {
+                throw new RuntimeException("Update has failed!! The Student has not been found!!" + student.getId());
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return student;
+
+
+    }
+
+    public List<Student> listAllStudents(Student student){
+        List<Student> students = new ArrayList<>();
+        String query = "SELECT * FROM students";
+        try (PreparedStatement statement = DataBaseConnectorConfig.getConnection().prepareStatement(query)){
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                student.setId(resultSet.getLong("id"));
+                student.setFullName(resultSet.getString("full_name"));
+                student.setGender(resultSet.getString("gender"));
+                student.setIdentityNo(resultSet.getString("identity_no"));
+                student.setEnrollmentDate(resultSet.getTimestamp("enrollment_date"));
+                student.setTotalCreditCount(resultSet.getInt("total_credit_count"));
+                student.setYearOfStudy(resultSet.getInt("year_of_study"));
+                students.add(student);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return students;
     }
 
 }
