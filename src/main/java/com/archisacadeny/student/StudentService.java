4dd4655c49c73c97b3278cd70700ba4d9e1e50ba
Görkem İskeçeli <gorkemiskeceli@Gorkem-MacBook-Air.local>
2024-07-02 19:59:42 +0300

    enrollment methods deleted

--- src/main/java/com/archisacadeny/student/StudentService.java
+++ src/main/java/com/archisacadeny/student/StudentService.java
@@ -6,28 +6,34 @@ public class StudentService {
     private final StudentRepository studentRepository;
 
 
-    public StudentService(StudentRepository studentRepository){
-        this.studentRepository=studentRepository;
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
-    public Student createStudent(Student student){
+
+    public Student createStudent(Student student) {
 
         return studentRepository.createStudent(student);
     }
-    public Student updateStudentInfo(Student student){
+
+    public Student updateStudentInfo(Student student) {
         return studentRepository.updateStudentInfo(student);
     }
-    public Student viewStudentDetails(Long studentId){
+
+    public Student viewStudentDetails(Long studentId) {
 
         Student savedStudent = studentRepository.viewStudentDetails(3);
         return savedStudent;
     }
-    public Student getStudentByID(long studentId){
+
+    public Student getStudentByID(long studentId) {
         return studentRepository.getStudentByID(studentId);
     }
-    public List<Student> getAllStudents(){
+
+    public List<Student> getAllStudents() {
         return studentRepository.listAllStudents();
     }
 
-    public void enrollStudentToCourse(long studentId, long courseId ){
+    public void enrollStudentToCourse(long studentId, long courseId) {
 
+    }
 }
