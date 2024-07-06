236eb3e2c62ad5319174863cb3ca1721550abc61
Orujhimaru <159823243+Orujhimaru@users.noreply.github.com>
2024-07-06 19:25:35 +0300

    removed main method

--- src/main/java/com/archisacadeny/Main.java
+++ src/main/java/com/archisacadeny/Main.java
@@ -22,27 +22,6 @@ public class Main {
     public static void main(String[] args) throws ParseException {
 
         DataBaseConnectorConfig.setConnection();
-        CourseService s = new CourseService(new CourseRepository());
-//        Course course = new Course();
-//        Instructor i = new Instructor();
-//        i.setId(1);
-//        course.setCourseName("Bootstrap");
-//        course.setCourseNumber("105");
-//        course.setInstructor(i);
-//        course.setDepartment("CSS");
-//        course.setCredits(5);
-//        course.setMaxStudents(5);
-//        course.setEnrolledStudents(new ArrayList<>());
-////                Course course = new Course();
-////                course.setId(2);
-//        s.update(course.getCourseNumber(),course);
-//          s.getTotalCreditAmount(1);
-//            s.calculateLetterGradeForStudent(1,1);
-//        s.getCoursesByInstructorId(1);
-//        s.getStudentCountForCourse(3);
-//        s.getAllCourses();
-//        s.listPopularCourses(1);
-//        s.generateCourseReport(4);
-//        s.calculateInstructorCoursesAttendanceRate(1);
+
     }
 }
