4e907a199384c4a1339227f0de0d7181f20e64a6
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-08-09 14:40:10 +0300

    ahmet/main-menu-start

--- src/main/java/com/archisacadeny/Main.java
+++ src/main/java/com/archisacadeny/Main.java
@@ -1,27 +1,142 @@
 package com.archisacadeny;
 
-import com.archisacadeny.config.DataBaseConfig;
 import com.archisacadeny.config.DataBaseConnectorConfig;
-import com.archisacadeny.course.Course;
 import com.archisacadeny.course.CourseRepository;
 import com.archisacadeny.course.CourseService;
-import com.archisacadeny.instructor.Instructor;
 import com.archisacadeny.instructor.InstructorRepository;
 import com.archisacadeny.instructor.InstructorService;
-import com.archisacadeny.person.Person;
-import com.archisacadeny.person.PersonRepository;
-import com.archisacadeny.person.PersonService;
 import com.archisacadeny.student.CourseStudentMapper;
 import com.archisacadeny.student.Student;
+import com.archisacadeny.student.StudentRepository;
+import com.archisacadeny.student.StudentService;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    private static CourseRepository courseRepository = new CourseRepository();
+    private static CourseStudentMapper courseStudentMapper = new CourseStudentMapper();
+    private static CourseService courseService = new CourseService(courseRepository, courseStudentMapper);
+    private static InstructorService instructorService = new InstructorService(new InstructorRepository(), courseRepository);
+    private static StudentService studentService = new StudentService(new StudentRepository());
+
+
     public static void main(String[] args) throws ParseException {
 
         DataBaseConnectorConfig.setConnection();
+
+        Scanner scanner = new Scanner(System.in);
+
+        studentService.createStudentTable();
+        instructorService.createInstructorTable();
+        courseService.createCourseTable();
+        CourseStudentMapper.createCourseStudentTable();
+
+        mainMenu(scanner, studentService, courseService, instructorService);
+    }
+
+    private static void mainMenu(Scanner scanner, StudentService studentService, CourseService courseService, InstructorService instructorService) {
+        System.out.println("Welcome to our University Management Panel!");
+
+        int choice;
+        do {
+            System.out.println("1. Student Operations");
+            System.out.println("2. Instructor Operations");
+            System.out.println("3. Course Operations");
+            System.out.println("4. Exit");
+            System.out.print("Your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    studentMenu(scanner, studentService);
+                    break;
+                case 2:
+                    instructorMenu(scanner, instructorService);
+                    break;
+                case 3:
+                    courseMenu(scanner, courseService);
+                    break;
+                case 4:
+                    System.out.println("Exit the panel...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        } while (true);
+    }
+
+    private static void studentMenu(Scanner scanner, StudentService studentService) {
+        System.out.println("1. Student Registration");
+        System.out.println("2. Student Deletion");
+        System.out.println("3. Create and print student success report");
+        System.out.println("4. Course recommendation for student");
+        System.out.println("5. Main menu");
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                Student student = new Student();
+
+                System.out.println("Enter the student's name: ");
+                scanner.nextLine();
+                String studentFullName = scanner.nextLine();
+                student.setFullName(studentFullName);
+
+
+                System.out.println("Enter the student's mail: ");
+                String studentMail = scanner.nextLine();
+                student.setEmail(studentMail);
+
+                System.out.println("Enter the student's gender: ");
+                String studentGender = scanner.nextLine();
+                student.setGender(studentGender);
+
+                System.out.println("Enter the student's identityNo: ");
+                String studentIdentity = scanner.nextLine();
+                student.setIdentityNo(studentIdentity);
+
+                Timestamp enrollmentDate = new Timestamp(System.currentTimeMillis());
+                student.setEnrollmentDate(enrollmentDate);
+                student.setTotalCreditCount(10);
+                student.setYearOfStudy(enrollmentDate.getYear());
+
+                studentService.createStudent(student);
+                break;
+            case 2:
+                System.out.println("Enter the ID of the student to be deleted: ");
+                int deletionStudentId = scanner.nextInt();
+                studentService.deleteStudent(deletionStudentId);
+                break;
+            case 3:
+                System.out.println("Enter the ID of the student for whom the report will be generated: ");
+                int studentIdForReport = scanner.nextInt();
+                studentService.printStudentAchievementReport(studentIdForReport);
+            case 4:
+                System.out.println("Enter the ID of the student to be recommended course: ");
+                int studentIdForRecommendedCourse = scanner.nextInt();
+                studentService.listRecommendedCoursesForStudent(studentIdForRecommendedCourse);
+                break;
+            case 5:
+                mainMenu(scanner, studentService, courseService, instructorService);
+                break;
+        }
+    }
+
+    private static void instructorMenu(Scanner scanner, InstructorService instructorService) {
+        System.out.println("1. Instructor Registration");
+        System.out.println("2. See all our instructors:");
+        System.out.println("3. Create and print student success report");
+        System.out.println("4. Course recommendation for student");
+        System.out.println("5. Main menu");
+        System.out.print("Your choice: ");
+    }
+
+    private static void courseMenu(Scanner scanner, CourseService courseService) {
 
     }
 }
