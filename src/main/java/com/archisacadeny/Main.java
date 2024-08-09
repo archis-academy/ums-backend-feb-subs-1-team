dfc3e555db8ea8969a534b8b595fde698d8b2edb
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-08-09 18:06:53 +0300

    ahmet/main-menu-update

--- src/main/java/com/archisacadeny/Main.java
+++ src/main/java/com/archisacadeny/Main.java
@@ -42,11 +42,14 @@ public class Main {
 
         int choice;
         do {
+            System.out.println("<---------------------------------->");
             System.out.println("1. Student Operations");
             System.out.println("2. Instructor Operations");
             System.out.println("3. Course Operations");
             System.out.println("4. Exit");
+            System.out.println("<---------------------------------->");
             System.out.print("Your choice: ");
+
             choice = scanner.nextInt();
 
             switch (choice) {
@@ -70,73 +73,156 @@ public class Main {
     }
 
     private static void studentMenu(Scanner scanner, StudentService studentService) {
-        System.out.println("1. Student Registration");
-        System.out.println("2. Student Deletion");
-        System.out.println("3. Create and print student success report");
-        System.out.println("4. Course recommendation for student");
-        System.out.println("5. Main menu");
-        System.out.print("Your choice: ");
-        int choice = scanner.nextInt();
 
-        switch (choice) {
-            case 1:
-                Student student = new Student();
+        System.out.println("Student Operations!");
+        System.out.println("<------------------------------------------>");
 
-                System.out.println("Enter the student's name: ");
-                scanner.nextLine();
-                String studentFullName = scanner.nextLine();
-                student.setFullName(studentFullName);
+        boolean backToMenu = false;
 
+        while (!backToMenu) {
+            System.out.println("1. Student Registration");
+            System.out.println("2. Student Deletion");
+            System.out.println("3. Create and print student success report");
+            System.out.println("4. Course recommendation for student");
+            System.out.println("0. Main menu");
+            System.out.print("Your choice: ");
 
-                System.out.println("Enter the student's mail: ");
-                String studentMail = scanner.nextLine();
-                student.setEmail(studentMail);
+            int choice = scanner.nextInt();
 
-                System.out.println("Enter the student's gender: ");
-                String studentGender = scanner.nextLine();
-                student.setGender(studentGender);
+            switch (choice) {
+                case 1:
+                    Student student = new Student();
 
-                System.out.println("Enter the student's identityNo: ");
-                String studentIdentity = scanner.nextLine();
-                student.setIdentityNo(studentIdentity);
+                    System.out.println("Enter the student's name: ");
+                    scanner.nextLine();
+                    String studentFullName = scanner.nextLine();
+                    student.setFullName(studentFullName);
 
-                Timestamp enrollmentDate = new Timestamp(System.currentTimeMillis());
-                student.setEnrollmentDate(enrollmentDate);
-                student.setTotalCreditCount(10);
-                student.setYearOfStudy(enrollmentDate.getYear());
+                    System.out.println("Enter the student's mail: ");
+                    String studentMail = scanner.nextLine();
+                    student.setEmail(studentMail);
 
-                studentService.createStudent(student);
-                break;
-            case 2:
-                System.out.println("Enter the ID of the student to be deleted: ");
-                int deletionStudentId = scanner.nextInt();
-                studentService.deleteStudent(deletionStudentId);
-                break;
-            case 3:
-                System.out.println("Enter the ID of the student for whom the report will be generated: ");
-                int studentIdForReport = scanner.nextInt();
-                studentService.printStudentAchievementReport(studentIdForReport);
-            case 4:
-                System.out.println("Enter the ID of the student to be recommended course: ");
-                int studentIdForRecommendedCourse = scanner.nextInt();
-                studentService.listRecommendedCoursesForStudent(studentIdForRecommendedCourse);
-                break;
-            case 5:
-                mainMenu(scanner, studentService, courseService, instructorService);
-                break;
+                    System.out.println("Enter the student's gender: ");
+                    String studentGender = scanner.nextLine();
+                    student.setGender(studentGender);
+
+                    System.out.println("Enter the student's identityNo: ");
+                    String studentIdentity = scanner.nextLine();
+                    student.setIdentityNo(studentIdentity);
+
+                    Timestamp enrollmentDate = new Timestamp(System.currentTimeMillis());
+                    student.setEnrollmentDate(enrollmentDate);
+                    student.setTotalCreditCount(10);
+                    student.setYearOfStudy(enrollmentDate.getYear());
+
+                    studentService.createStudent(student);
+                    break;
+                case 2:
+                    System.out.println("Enter the ID of the student to be deleted: ");
+                    int deletionStudentId = scanner.nextInt();
+                    studentService.deleteStudent(deletionStudentId);
+                    break;
+                case 3:
+                    System.out.println("Enter the ID of the student for whom the report will be generated: ");
+                    int studentIdForReport = scanner.nextInt();
+                    studentService.printStudentAchievementReport(studentIdForReport);
+                    break;
+                case 4:
+                    System.out.println("Enter the ID of the student to be recommended course: ");
+                    int studentIdForRecommendedCourse = scanner.nextInt();
+                    studentService.listRecommendedCoursesForStudent(studentIdForRecommendedCourse);
+                    break;
+                case 0:
+                    System.out.println("Returning to the main menu...");
+                    backToMenu = true;
+                    break;
+                default:
+                    System.out.println("Wrong choice!");
+                    break;
+            }
         }
     }
 
     private static void instructorMenu(Scanner scanner, InstructorService instructorService) {
-        System.out.println("1. Instructor Registration");
-        System.out.println("2. See all our instructors:");
-        System.out.println("3. Create and print student success report");
-        System.out.println("4. Course recommendation for student");
-        System.out.println("5. Main menu");
-        System.out.print("Your choice: ");
+
+        System.out.println("Instructor Operations!");
+        System.out.println("<------------------------------------------>");
+
+        boolean backToMenu = false;
+
+        while (!backToMenu) {
+            System.out.println("1. Instructor Registration");
+            System.out.println("2. See all our instructors:");
+            System.out.println("3. Create and print student success report");
+            System.out.println("4. Course recommendation for student");
+            System.out.println("0. Main menu");
+            System.out.print("Your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 0:
+                    System.out.println("Returning to the main menu...");
+                    backToMenu = true;
+                    break;
+                default:
+                    System.out.println("Wrong choice!");
+                    break;
+            }
+        }
     }
 
     private static void courseMenu(Scanner scanner, CourseService courseService) {
+
+        System.out.println("Course Operations!");
+        System.out.println("<------------------------------------------>");
+
+        boolean backToMenu = false;
+
+        while (!backToMenu) {
+            System.out.println("1. Course Registration");
+            System.out.println("2. See all our courses:");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("0. Main menu");
+            System.out.print("Your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 0:
+                    System.out.println("Returning to the main menu...");
+                    backToMenu = true;
+                    break;
+                default:
+                    System.out.println("Wrong choice!");
+                    break;
+            }
+        }
 
     }
 }
